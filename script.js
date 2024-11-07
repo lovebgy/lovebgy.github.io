2aee00975054eae747b50818dceb480d30105e67
lovebgy <wrf_bgy@163.com>
2024-11-07 17:21:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ window.onload = function() {
     const differenceInMilliseconds = currentDate - targetDate; // 时间差（毫秒）
     const differenceInDays = Math.floor(differenceInMilliseconds / (1000 * 60 * 60 * 24)); // 转换为天数
 
-    document.getElementById('daysSince').textContent = `It has been ${differenceInDays} days since October 1, 2021.`; // 显示天数
+    document.getElementById('daysSince').textContent = `我们在一起 ${differenceInDays} 天啦！`; // 显示天数
 };
